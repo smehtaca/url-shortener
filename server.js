0d601264087bb32b9dc1ee2a47c15b6d09394cc4
Sameer Mehta <smehtaca@users.noreply.github.com>
2017-12-25 21:20:25 -0500

    Created tests for url encoding and decoding, created url encode and decode functions

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 
 // Require App
-const server = require("./app");
+const server = require("../src/app");
 
 server.listen(3000, function() {
   console.log("Server listening on port 3000");
---
+++ tests/url.test.js
@@ -0,0 +1,21 @@
+//Tests for URL encoding and decoding go here
+
+const url = require("../src/url");
+
+describe("Test the url encode and decode functions", () => {
+  test("URL encode should return an encoded string", () => {
+    let num = 9999;
+    let base58 = "3Yp";
+    let result = url.encode(num);
+    return expect(result).toBe(base58);
+  });
+});
+
+describe("Test the url encode and decode functions", () => {
+  test("URL decode should return a decoded integere", () => {
+    let base58 = "3Yp";
+    let num = 9999;
+    let result = url.decode(base58);
+    return expect(result).toBe(num);
+  });
+});
