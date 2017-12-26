0ef6159c99b9fa06bd99e5ebd090b6c99d6b8aec
Sameer Mehta <smehtaca@users.noreply.github.com>
2017-12-25 20:03:35 -0500

    Updated app tests and created homepage to serve at root route

--- app.js
+++ app.js
@@ -4,5 +4,15 @@
 const express = require("express");
 // Init App
 const app = express();
+// Path Module for concatenating file paths
+const path = require("path");
+
+// Serve public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Serve up homepage
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"));
+});
 
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -1,16 +1,42 @@
 // app.test.js
 // Tests for api endpoints go here
-
+// Require puppeteer for UI Testing
+const puppeteer = require("puppeteer");
 // Require supertest
 const request = require("supertest");
-
 //Require App
 const app = require("./app");
 
-describe("Test the root path", () => {
-  test("It should return the GET method", () => {
-    return request(app)
-      .get("/")
-      .expect(200);
-  });
+let page;
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000/");
+});
+afterAll(() => {
+  browser.close();
+});
+
+describe("Test the frontend", () => {
+  test(
+    "Header should be URL Shortener",
+    async () => {
+      const title = await page.title();
+      expect(title).toBe("URL Shortener");
+    },
+    25000
+  );
+});
+
+describe("Test the frontend", () => {
+  test(
+    "Check that a url input field exists",
+    async () => {
+      const urlElement = await page.$eval("#url", el => (el ? true : false));
+      expect(urlElement).toBe(true);
+    },
+    25000
+  );
 });
